c0e9d1742b3d88c5052dfb29ad00b291ab28d89c
Kishan <kishangupta21995@gmail.com>
2019-12-10 15:06:17 -0500

    Profile Activity

--- app/src/main/java/com/app/gateway/activities/MaidProfile1Activity.java
+++ app/src/main/java/com/app/gateway/activities/MaidProfile1Activity.java
@@ -2,15 +2,42 @@ package com.app.gateway.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import com.app.gateway.R;
 
 public class MaidProfile1Activity extends AppCompatActivity {
+    private ImageView _backIMG;
+    private Button _showAll;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maid_profile1);
+
+        _backIMG=findViewById(R.id.backIMG);
+        _showAll=findViewById(R.id.showAll);
+
+
+        _backIMG.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MaidProfile1Activity.this,MaidActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        _showAll.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MaidProfile1Activity.this,ReviewActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
