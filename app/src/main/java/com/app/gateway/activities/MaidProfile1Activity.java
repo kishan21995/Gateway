6aaee6b93568010cbba98b3be52605626f014063
Kishan <kishangupta21995@gmail.com>
2019-12-14 15:42:36 -0500

    Add Call Activity

--- app/src/main/java/com/app/gateway/activities/MaidProfile1Activity.java
+++ app/src/main/java/com/app/gateway/activities/MaidProfile1Activity.java
@@ -3,6 +3,7 @@ package com.app.gateway.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -11,7 +12,7 @@ import android.widget.ImageView;
 import com.app.gateway.R;
 
 public class MaidProfile1Activity extends AppCompatActivity {
-    private ImageView _backIMG;
+    private ImageView _backIMG,starIMG;
     private Button _showAll;
 
     @Override
@@ -21,6 +22,7 @@ public class MaidProfile1Activity extends AppCompatActivity {
 
         _backIMG=findViewById(R.id.backIMG);
         _showAll=findViewById(R.id.showAll);
+        starIMG=findViewById(R.id.starIMG);
 
 
         _backIMG.setOnClickListener(new View.OnClickListener() {
@@ -35,6 +37,15 @@ public class MaidProfile1Activity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intent=new Intent(MaidProfile1Activity.this,ReviewActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        starIMG.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:06396746908"));
                 startActivity(intent);
             }
         });
