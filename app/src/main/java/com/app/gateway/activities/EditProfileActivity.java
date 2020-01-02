4bf94d2d0590348defea9a8bace30c981f14a519
Kishan <kishangupta21995@gmail.com>
2020-01-02 16:17:32 -0500

    Update Edit profile

--- app/src/main/java/com/app/gateway/activities/EditProfileActivity.java
+++ app/src/main/java/com/app/gateway/activities/EditProfileActivity.java
@@ -11,6 +11,11 @@ import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.app.gateway.R;
+import com.app.gateway.models.login.LoginResponse;
+import com.app.gateway.utils.HFCPrefs;
+import com.google.gson.Gson;
+
+import static com.app.gateway.utils.Constants.LOGIN_DATA;
 
 public class EditProfileActivity extends AppCompatActivity {
     private ImageView _backIMG;
@@ -23,9 +28,9 @@ public class EditProfileActivity extends AppCompatActivity {
         setContentView(R.layout.activity_edit_profile);
 
         _backIMG=findViewById(R.id.backIMG);
-        _nameET=findViewById(R.id.nameET);
+        _nameET=findViewById(R.id.usernameET);
         _phoneET=findViewById(R.id.phoneET);
-        _emailET=findViewById(R.id.emailET1);
+        _emailET=findViewById(R.id.emailET);
         _saveBTN=findViewById(R.id.saveBTN);
 
         _backIMG.setOnClickListener(new View.OnClickListener() {
@@ -42,6 +47,38 @@ public class EditProfileActivity extends AppCompatActivity {
                 Validation();
             }
         });
+
+
+        LoginResponse loginResponse = new Gson().fromJson(HFCPrefs.getString(this,LOGIN_DATA),LoginResponse.class);
+
+        //Log.d("mdfnv", "onCreate: "+loginResponses);
+
+
+
+        if (loginResponse.getUsername()!=null ){
+            _nameET.setText(loginResponse.getUsername() );
+        }else {
+            _nameET.setText(" " );
+        }
+
+        if (loginResponse.getEmail()!=null ){
+            _emailET.setText(loginResponse.getEmail() );
+        }else {
+            _emailET.setText(" " );
+        }
+
+        if (loginResponse.getMobile()!=null ){
+            _phoneET.setText(loginResponse.getMobile() );
+        }else {
+            _phoneET.setText(" " );
+        }
+
+      /*  if (loginResponse.getPasscode()!=null ){
+            _usernamePASSCODE.setText(loginResponse.getPasscode() );
+        }else {
+            _usernamePASSCODE.setText(" " );
+        }*/
+
     }
     public void Validation() {
         boolean check = true;
